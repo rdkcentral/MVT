854cfc8e8a1f401e382ecdd820d68c5c368e2be1
lakshya-chauhan-infosys <lchauhan.ext@libertyglobal.com>
2026-07-08 11:42:39 +0530

    Fixed the key roation error

--- src/mediaStreams.js
+++ src/mediaStreams.js
@@ -505,7 +505,7 @@ var MS = {
       variant: "dash",
       container: "fmp4",
       note: "PlayReady 4.0 with key rotation",
-      src: "https://d24rwxnt7vw9qb.cloudfront.net/v1/dash/e6d234965645b411ad572802b6c9d5a10799c9c1/All_Reference_Streams/2fc23947945841b9b1be9768f9c13e75/index.mpd%22",
+      src: "https://d24rwxnt7vw9qb.cloudfront.net/v1/dash/e6d234965645b411ad572802b6c9d5a10799c9c1/All_Reference_Streams/2fc23947945841b9b1be9768f9c13e75/index.mpd",
       video: {
         codec: "avc",
       },
@@ -517,7 +517,8 @@ var MS = {
           "com.widevine.alpha": {
             serverURL: "https://lic.staging.drmtoday.com/license-proxy-widevine/cenc/?specConform=true",
             httpRequestHeaders: {
-              "x-dt-custom-data":"ewogICAgInVzZXJJZCI6ICJhd3MtZWxlbWVudGFsOjpzcGVrZS10ZXN0aW5nIiwKICAgICJzZXNzaW9uSWQiOiAiZWxlbWVudGFsLXJlZnN0cmVhbSIsCiAgICAibWVyY2hhbnQiOiAiYXdzLWVsZW1lbnRhbCIKfQo=",
+              "x-dt-custom-data":
+                "ewogICAgInVzZXJJZCI6ICJhd3MtZWxlbWVudGFsOjpzcGVrZS10ZXN0aW5nIiwKICAgICJzZXNzaW9uSWQiOiAiZWxlbWVudGFsLXJlZnN0cmVhbSIsCiAgICAibWVyY2hhbnQiOiAiYXdzLWVsZW1lbnRhbCIKfQo=",
             },
           },
           "com.microsoft.playready": {
